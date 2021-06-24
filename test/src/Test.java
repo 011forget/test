e134d9ae36a18db842442481e2da978f943a55f9
ligang <endeavor0@126.com>
2021-06-24 11:22:49 +0800

    修改123三个打印

--- test/src/Test.java
+++ test/src/Test.java
@@ -6,5 +6,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("10000000");
         System.out.println("10000000");
+        System.out.println("10000000");
+        System.out.println("10000000");
     }
 }
